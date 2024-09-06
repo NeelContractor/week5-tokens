ea1d2b73af9bb4974b614331e416ddfe36d4646a
NeelContractor <neelcontractor7@gmail.com>
2024-09-06 13:44:47 +0530

    pushing code to github

---
+++ components/CreateToken.tsx
@@ -0,0 +1,210 @@
+"use client";
+import bs58 from "bs58";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { Files } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { createMint, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import toast from "react-hot-toast";
+import { INewToken } from "@/types";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+declare global {
+  interface Window {
+    solana: any;
+  }
+}
+
+const CreateToken = ({
+  newTokenDetails,
+  setNewTokenDetails,
+  isLoading,
+  setIsLoading,
+}: {
+  newTokenDetails: INewToken | null;
+  setNewTokenDetails: React.Dispatch<React.SetStateAction<INewToken | null>>;
+  isLoading: boolean;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const [decimals, setDecimals] = useState(9);
+  const [freezeAuthorityPublicKey, setFreezeAuthorityPublicKey] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleAirdrop = async () => {
+    if (!window.solana || !window.solana.isPhantom) {
+      toast.error("No Phantom Wallet found, please install Phantom");
+      return;
+    }
+    const toastId = toast.loading("Airdropping 2 SOL");
+    setIsLoading(true);
+    try {
+      const phantomPublicKey = await connectPhantom();
+      const airdropSignature = await connection.requestAirdrop(phantomPublicKey, 2 * LAMPORTS_PER_SOL);
+      const data = await connection.confirmTransaction(airdropSignature);
+      toast.success("Airdrop successful", { id: toastId });
+    } catch (e: any) {
+      console.log(e?.error);
+      toast.error("Something went wrong", { id: toastId });
+    }
+    setIsLoading(false);
+  };
+
+  async function connectPhantom() {
+    if (window.solana && window.solana.isPhantom) {
+      try {
+        const response = await window.solana.connect();
+        console.log("Connected with Public Key:", response.publicKey.toString());
+        return response.publicKey;
+      } catch (err) {
+        console.error("Connection failed:", err);
+      }
+    } else {
+      console.log("Phantom Wallet not found.");
+    }
+  }
+
+  const handleCreate = async () => {
+    if (!decimals) {
+      toast.error("Please fill all required fields");
+      return;
+    }
+
+    if (!window.solana || !window.solana.isPhantom) {
+      toast.error("No Phantom Wallet found, please install Phantom");
+      return;
+    }
+
+    const toastId = toast.loading("Awaiting Transaction...");
+    setIsLoading(true);
+    try {
+      const mintWallet = Keypair.generate();
+      const phantomPublicKey = await connectPhantom();
+      const freezeAuthority = freezeAuthorityPublicKey === "" ? null : new PublicKey(freezeAuthorityPublicKey);
+
+      const { blockhash } = await connection.getLatestBlockhash("finalized");
+      const transaction = new Transaction({
+        recentBlockhash: blockhash,
+        feePayer: phantomPublicKey,
+      }).add(
+        SystemProgram.transfer({
+          fromPubkey: phantomPublicKey,
+          toPubkey: new PublicKey(mintWallet.publicKey),
+          lamports: LAMPORTS_PER_SOL * 0.2,
+        })
+      );
+
+      const { signature } = await window.solana.signAndSendTransaction(transaction);
+      const result = await connection.confirmTransaction(signature, "confirmed");
+
+      if (result.value.err) throw new Error("Transaction via Phantom failed");
+
+      toast.loading("Transaction successful, creating Token...", { id: toastId });
+
+      const mint = await createMint(connection, mintWallet, mintWallet.publicKey, freezeAuthority, decimals, undefined, undefined, TOKEN_2022_PROGRAM_ID);
+      toast.success("Token created successfully", { id: toastId });
+
+      setNewTokenDetails({
+        mintWallet: bs58.encode(mintWallet.secretKey),
+        tokenMint: mint.toBase58(),
+      });
+    } catch (e) {
+      console.log(e);
+      toast.error("Something went wrong", { id: toastId });
+    }
+    setIsLoading(false);
+  };
+
+  const setPhantomWalletAsFreezeAuthority = async () => {
+    if (!window.solana || !window.solana.isPhantom) {
+      toast.error("No Phantom Wallet found, please install Phantom");
+      return;
+    }
+    const phantomPublicKey = await connectPhantom();
+    setFreezeAuthorityPublicKey(phantomPublicKey);
+  };
+
+  useEffect(() => {
+    if (isChecked) setPhantomWalletAsFreezeAuthority();
+    else setFreezeAuthorityPublicKey("");
+  }, [isChecked]);
+
+  return (
+    <div className="w-full">
+      {newTokenDetails ? (
+        <>
+          <CreateSuccess newTokenDetails={newTokenDetails} setNewTokenDetails={setNewTokenDetails} />
+        </>
+      ) : (
+        <div className="w-full min-h-[500px] flex flex-col justify-between items-center">
+          <div className="flex flex-col gap-4 w-full">
+            <div>
+              <p className="text-sm text-gray-950 mb-2">Decimals *</p>
+              <Input type="number" value={decimals} onChange={(e) => setDecimals(e.target.valueAsNumber)} className="text-lime-300" />
+            </div>
+            <div>
+              <div className="flex justify-between mb-2">
+                <p className="text-sm text-gray-950">Freeze Authority Public Key</p>
+              </div>
+              <Input value={freezeAuthorityPublicKey} onChange={(e) => setFreezeAuthorityPublicKey(e.target.value)} className="text-lime-300" />
+              <div className="flex gap-2 items-center w-full justify-end mt-2">
+                <Checkbox checked={isChecked} onCheckedChange={() => setIsChecked((prev) => !prev)} />
+                <label htmlFor="terms" className="text-sm text-gray-950">
+                  Same as Wallet
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="w-full mt-4">
+            <div className="w-full flex flex-col gap-2 lg:flex-row">
+              <Button disabled={isLoading} className="w-full bg-gray-950 text-lime-300 hover:bg-gray-950 hover:text-lime-300" onClick={handleAirdrop}>
+                Airdrop 2 SOL In Phantom
+              </Button>
+              <Button disabled={isLoading} className="w-full bg-gray-950 text-lime-300 hover:bg-gray-950 hover:text-lime-300" onClick={handleCreate}>
+                Create Token (0.2 SOL)
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const CreateSuccess = ({ newTokenDetails, setNewTokenDetails }: { newTokenDetails: INewToken; setNewTokenDetails: React.Dispatch<React.SetStateAction<INewToken | null>> }) => {
+  const handleCopy = (text: string) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => toast.success(`Copied to clipboard!`))
+      .catch(() => toast.error(`Failed to copy`));
+  };
+
+  return (
+    <div className="w-full min-h-[500px] flex flex-col">
+      <div className="flex-grow flex flex-col mt-4 gap-4">
+        <p className="font-black text-4xl tracking-tight text-center mb-4 text-black">Token Created Successfully!!!</p>
+        <div className="w-full bg-gray-950 rounded p-4">
+          <div className="w-full flex justify-center items-center mb-2 gap-2">
+            <p className="font-light text-sm text-lime-300">New Token(Mint) Id</p>
+            <Files size={20} className="cursor-pointer text-lime-300" onClick={() => handleCopy(newTokenDetails.tokenMint)} />
+          </div>
+          <p className="font-mono font-bold text-lime-300 text-ellipsis overflow-clip">{newTokenDetails.tokenMint}</p>
+        </div>
+        <div className="w-full bg-gray-950 rounded p-4">
+          <div className="w-full flex justify-center items-center mb-2 gap-2">
+            <p className="font-light text-sm text-lime-300">Mint Wallet Private Key</p>
+            <Files size={20} className="cursor-pointer text-lime-300" onClick={() => handleCopy(newTokenDetails.mintWallet)} />
+          </div>
+          <p className="font-mono font-bold text-ellipsis overflow-clip text-lime-300">{newTokenDetails.mintWallet}</p>
+        </div>
+      </div>
+      <Button onClick={() => setNewTokenDetails(null)} className="w-full bg-gray-950 text-lime-300 hover:bg-gray-950 hover:text-lime-300">
+        Create New Token
+      </Button>
+    </div>
+  );
+};
+
+export default CreateToken;
---
+++ components/MintToken.tsx
@@ -0,0 +1,189 @@
+
+"use client";
+import bs58 from "bs58";
+import { Button } from "./../components/ui/button";
+import { Checkbox } from "./../components/ui/checkbox";
+import { Input } from "./../components/ui/input";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { CircleDollarSign, RefreshCcw } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { getOrCreateAssociatedTokenAccount, mintTo, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import toast from "react-hot-toast";
+import CompactToolTip from "./../components/ui/CompactToolTip";
+import { INewToken } from "../types";
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+const MintToken = ({ newTokenDetails, isLoading, setIsLoading }: { newTokenDetails: INewToken | null; isLoading: boolean; setIsLoading: React.Dispatch<React.SetStateAction<boolean>> }) => {
+  const [walletPrivateKey, setWalletPrivateKey] = useState("");
+  const [balance, setBalance] = useState(0);
+  const [tokenKey, setTokenKey] = useState("");
+  const [recipeintPublicKey, setRecipientPublicKey] = useState("");
+  const [amount, setAmount] = useState(100);
+  const [isUsePhantomChecked, setIsUsePhantomChecked] = useState(false);
+  const [isUseNewlyCreatedToken, setIsUseNewlyCreatedToken] = useState(false);
+
+  const fetchBalance = async () => {
+    if (isLoading) return;
+    if (!walletPrivateKey || walletPrivateKey === "") {
+      toast.error("Please provide wallet");
+      return;
+    }
+    const toastId = toast.loading("Fetching Balance");
+    setIsLoading(true);
+    try {
+      const secretKey = bs58.decode(walletPrivateKey);
+      const payer = Keypair.fromSecretKey(secretKey);
+      const publicKey = payer.publicKey;
+      const balance = await connection.getBalance(publicKey);
+      toast.success("Balance fetched successfully", { id: toastId });
+      setBalance(balance);
+    } catch (error) {
+      toast.error("Error in fetching balance", { id: toastId });
+    }
+    setIsLoading(false);
+  };
+
+  const handleAirdrop = async () => {
+    if (isLoading) return;
+    if (!walletPrivateKey || walletPrivateKey === "") {
+      toast.error("Please provide wallet");
+      return;
+    }
+    const toastId = toast.loading("Airdropping 2 SOL");
+    setIsLoading(true);
+    try {
+      const secretKey = bs58.decode(walletPrivateKey);
+      const payer = Keypair.fromSecretKey(secretKey);
+      const airdropSignature = await connection.requestAirdrop(payer.publicKey, 2e9);
+      const data = await connection.confirmTransaction(airdropSignature);
+      toast.success("Airdrop successful", { id: toastId });
+      fetchBalance();
+    } catch (e) {
+      toast.error("Something went wrong", { id: toastId });
+    }
+    setIsLoading(false);
+  };
+
+  const handleMint = async () => {
+    if (!walletPrivateKey || !tokenKey || !amount || !recipeintPublicKey) {
+      toast.error("Please fill all required fields");
+      return;
+    }
+    const toastId = toast.loading("Minting Token");
+    setIsLoading(true);
+    try {
+      const secretKey = bs58.decode(walletPrivateKey);
+      const payer = Keypair.fromSecretKey(secretKey);
+      const tokenPublicKey = new PublicKey(tokenKey);
+      const tokenAccount = await getOrCreateAssociatedTokenAccount(connection, payer, tokenPublicKey, new PublicKey(recipeintPublicKey), false, undefined, undefined, TOKEN_2022_PROGRAM_ID);
+      const data = await mintTo(connection, payer, tokenPublicKey, tokenAccount.address, payer, amount * LAMPORTS_PER_SOL, undefined, undefined, TOKEN_2022_PROGRAM_ID);
+      toast.success("Token minted successfully, check your wallet", { id: toastId });
+    } catch (e) {
+      console.log(e);
+      toast.error("Something went wrong", { id: toastId });
+    }
+    setIsLoading(false);
+  };
+
+  async function connectPhantom() {
+    if (window.solana && window.solana.isPhantom) {
+      try {
+        const response = await window.solana.connect();
+        console.log("Connected with Public Key:", response.publicKey.toString());
+        return response.publicKey;
+      } catch (err) {
+        console.error("Connection failed:", err);
+      }
+    } else {
+      console.log("Phantom Wallet not found.");
+    }
+  }
+
+  const setPhantomWalletAsRecipient = async () => {
+    if (!window.solana || !window.solana.isPhantom) {
+      toast.error("No Phantom Wallet found, please install Phantom");
+      return;
+    }
+    const phantomPublicKey = await connectPhantom();
+    setRecipientPublicKey(phantomPublicKey);
+  };
+
+  useEffect(() => {
+    if (isUsePhantomChecked) setPhantomWalletAsRecipient();
+    else setRecipientPublicKey("");
+  }, [isUsePhantomChecked]);
+
+  useEffect(() => {
+    if (isUseNewlyCreatedToken && newTokenDetails) {
+      setWalletPrivateKey(newTokenDetails.mintWallet);
+      setTokenKey(newTokenDetails.tokenMint);
+    } else {
+      setWalletPrivateKey("");
+      setTokenKey("");
+    }
+  }, [isUseNewlyCreatedToken]);
+
+  useEffect(() => {
+    if (newTokenDetails !== null) setIsUseNewlyCreatedToken(true);
+  }, [newTokenDetails]);
+
+  return (
+    <div className="w-full h-[500px] flex flex-col justify-between">
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-2 items-center w-full mt-2">
+          <Checkbox disabled={newTokenDetails === null} checked={isUseNewlyCreatedToken} onCheckedChange={() => setIsUseNewlyCreatedToken((prev) => !prev)} id="terms" />
+          <label htmlFor="terms" className={`text-sm ${newTokenDetails === null ? "text-lime-800" : "text-gray-950"}`}>
+            Use newly created token
+          </label>
+        </div>
+        {!isUseNewlyCreatedToken && (
+          <>
+            <div>
+              <p className="text-sm text-gray-950 mb-1">Mint Private Key *</p>
+              <Input
+                value={walletPrivateKey}
+                onChange={(e) => {
+                  setWalletPrivateKey(e.target.value);
+                }}
+                className="text-lime-300"
+              />
+              <div className="mt-1 flex gap-2 items-center">
+                <p className="text-sm text-gray-950">Balance: {balance / LAMPORTS_PER_SOL} SOL</p>
+                <CompactToolTip component={<RefreshCcw onClick={fetchBalance} size={15} className="text-gray-950 cursor-pointer" />} title="Refresh" />
+                <CompactToolTip component={<CircleDollarSign onClick={handleAirdrop} size={17} className="text-gray-950 cursor-pointer" />} title="Airdrop 2 SOL" />
+              </div>
+            </div>
+            <div>
+              <p className="text-sm text-gray-950 mb-1">Token Key *</p>
+              <Input value={tokenKey} onChange={(e) => setTokenKey(e.target.value)} className="text-lime-300" />
+            </div>
+          </>
+        )}
+        <div>
+          <p className="text-sm text-gray-950 mb-1">Amount</p>
+          <Input type="number" value={amount} onChange={(e) => setAmount(e.target.valueAsNumber)} className="text-lime-300" />
+        </div>
+        <div>
+          <div className="flex justify-between mb-1">
+            <p className="text-sm text-gray-950">Recipient Address *</p>
+          </div>
+          <Input value={recipeintPublicKey} onChange={(e) => setRecipientPublicKey(e.target.value)} className="text-lime-300" />
+          <div className="flex gap-2 items-center w-full justify-end mt-2">
+            <Checkbox checked={isUsePhantomChecked} onCheckedChange={() => setIsUsePhantomChecked((prev) => !prev)} id="terms" />
+            <label htmlFor="terms" className="text-sm text-gray-950">
+              In Phantom Wallet
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <Button disabled={isLoading} className="w-full bg-gray-950 text-lime-300 hover:bg-gray-950 hover:text-lime-300" onClick={handleMint}>
+          Mint Token
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default MintToken;
